3aa6e729718408a5acf9a3387eb654a9986698bb
VladislavSurkov <fvhabjrega@gmail.com>
2023-03-15 11:52:25 +0200

    update

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -10,84 +10,60 @@ const {
 const { ServerError } = require("../helpers/error");
 
 const getContactController = async (req, res, next) => {
-  try {
-    const contacts = await listContacts();
-    res.json(contacts);
-  } catch (err) {
-    throw new ServerError(`${err.message}`, 500);
-  }
+  const contacts = await listContacts();
+  res.json(contacts);
 };
 
 const getContactByIdController = async (req, res, next) => {
-  try {
-    const contactById = await getContactById(req.params.contactId);
+  const contactById = await getContactById(req.params.contactId);
 
-    contactById
-      ? res.json(contactById)
-      : res.status(404).json({ massage: "Not found" });
-  } catch (err) {
-    throw new ServerError(`${err.message}`, 500);
-  }
+  contactById
+    ? res.json(contactById)
+    : res.status(404).json({ massage: "Not found" });
 };
 
 const addContactControler = async (req, res, next) => {
-  try {
-    const { name, email, phone } = req.body;
-    const newContact = await addContact({ name, email, phone });
+  const { name, email, phone, favorite } = req.body;
+  const newContact = await addContact({ name, email, phone, favorite });
 
-    res.status(201).json(newContact);
-  } catch (err) {
-    throw new ServerError(`${err.message}`, 500);
-  }
+  res.status(201).json(newContact);
 };
 
 const deleteContactControler = async (req, res, next) => {
-  try {
-    const contacts = await removeContact(req.params.contactId);
+  const contacts = await removeContact(req.params.contactId);
 
-    contacts
-      ? res.json({ message: "contact deleted" })
-      : res.status(404).json({ massage: "Not found" });
-  } catch (err) {
-    throw new ServerError(`${err.message}`, 500);
-  }
+  contacts
+    ? res.json({ message: "contact deleted" })
+    : res.status(404).json({ massage: "Not found" });
 };
 
 const updateContactsControler = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const { name, email, phone } = req.body;
-    const updatedContact = await updateContact(contactId, {
-      name,
-      email,
-      phone,
-    });
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+  const updatedContact = await updateContact(contactId, {
+    name,
+    email,
+    phone,
+  });
 
-    updatedContact
-      ? res.json(updatedContact)
-      : res.status(404).json({ massage: "Not found" });
-  } catch (err) {
-    throw new ServerError(`${err.message}`, 500);
-  }
+  updatedContact
+    ? res.json(updatedContact)
+    : res.status(404).json({ massage: "Not found" });
 };
 
 const updateStatusControler = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const { favorite } = req.body;
+  const { contactId } = req.params;
+  const { favorite } = req.body;
 
-    if (favorite === undefined) {
-      throw new ServerError("missing field favorite", 400);
-    }
-
-    const updatedStatus = await updateStatusContact(contactId, { favorite });
-
-    updatedStatus
-      ? res.json(updatedStatus)
-      : res.status(404).json({ message: `Not found` });
-  } catch (err) {
-    throw new ServerError(`${err.message}`, 500);
+  if (favorite === undefined) {
+    throw new ServerError("missing field favorite", 400);
   }
+
+  const updatedStatus = await updateStatusContact(contactId, { favorite });
+
+  updatedStatus
+    ? res.json(updatedStatus)
+    : res.status(404).json({ message: `Not found` });
 };
 
 module.exports = {
